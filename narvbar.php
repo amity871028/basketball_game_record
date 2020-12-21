26f17a6cb80eb185ed78baf0df9e157d0d4a61fa
amity <amity3744039@gmail.com>
2020-12-21 14:28:12 +0800

    fix: modify file site & add gitignore

--- narvbar.php
+++ narvbar.php
@@ -8,14 +8,3 @@
         </div>
     </div>
 </nav>
-<?php
-  $user = 'root'; //資料庫使用者名稱
-  $password = '12345789'; //資料庫的密碼
-  try{
-      $db = new PDO('mysql:host=localhost;dbname=basketball_game_record;charset=utf8',$user,$password);
-      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      $db->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
-  }catch(PDOException $e){ 
-      Print "ERROR!: " . $e->getMessage();
-  die();
-  }
